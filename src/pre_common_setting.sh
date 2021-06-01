f88564597a677200de5489f374da3f23cbd1d00e
chunlei.luo <chunlei.luo@goldenrivertek.com>
2021-06-01 15:49:03 +0800

    加入我自己实现的 my shell=mysh.

---
+++ src/pre_common_setting.sh
@@ -0,0 +1,47 @@
+# use the original bashrc file
+source $HOME/.bashrc
+
+# below are the customized settings.
+
+function mysh_prompt()
+{
+    info=""
+
+    # find ss-local
+    ss_pid=`pidof ss-local 2>/dev/null`
+    if [ -n "$ss_pid" ]; then
+        local cmd=`ps -q ${ss_pid} -o args= | awk '{print $3}'`
+        info="${info}CNT=${cmd##*\.} "
+    else
+        info="${info}CNT=OFF "
+    fi
+
+    # show proxy
+    if [ -n "$http_proxy" ]; then
+        info="${info}PROXY=ON "
+    else
+        info="${info}PROXY=OFF "
+    fi
+
+    echo "$info"
+}
+
+# 参考 http://billie66.github.io/TLCL/book/chap14.html
+#color "\[\e[S;F;Bm\]", "\[\e[0m\]" 取消颜色。
+#前景F 背景B 颜色
+#30 40 黑色
+#31 41 红色
+#32 42 绿色
+#33 43 黃色
+#34 44 蓝色
+#35 45 紫紅色
+#36 46 青蓝色
+#37 47 白色
+# S: 0、  1、   22、    4、     24、      5、   25、     7、   27， 
+# 默认值、粗体、非粗体、下划线、非下划线、闪烁、非闪烁、 反显、非反显。
+
+#PS1='\342\224\214 \[\e[1;37;42m\][#\#@\!| \t| JOB:\j| \w]\[\e[0m\]\n\342\224\224 `if [ $? -eq 0 ]; then echo -n O; else echo -n X; fi;` $(mysh_prompt)\n\$ '
+# 这里必须用单引号，否则"$?"就无法推迟到显示时才执行。
+MYSH_CMD_RLT='`if [ $? -eq 0 ]; then echo -n "\[\e[1;37;42m\]O\[\e[0m\]"; else echo -n "\[\e[1;37;41m\]X\[\e[0m\]"; fi;`'
+MYSH_PROMPT='$(mysh_prompt)'
+PS1="\342\224\214 $MYSH_CMD_RLT \[\e[1;37;42m\][#\#@\! \t JOB:\j \w]\[\e[0m\]\n\342\224\224   \[\e[1;37;43m\]$MYSH_PROMPT\[\e[0m\]\n\$ "
---
+++ src/shell.sh
@@ -0,0 +1,6 @@
+
+source ${MYSH_SRC_DIR}/pre_common_setting.sh
+source ${MYSH_COMMON_SETTING}
+
+source ${MYSH_SRC_DIR}/pre_cur_setting.sh
+source ${MYSH_CUR_SETTING}
