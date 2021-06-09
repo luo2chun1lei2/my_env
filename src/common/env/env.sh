269c4a2c5ff93dca93ea093570d2f1a8c7e69ace
chunlei.luo <chunlei.luo@goldenrivertek.com>
2021-06-09 09:44:37 +0800

    fix: awk 处理路径的问题。

--- src/common/env/env.sh
+++ src/common/env/env.sh
@@ -45,12 +45,19 @@ function myenv_set_name_of_env_path()
 		echo "Cannot get full path of $path"
 		return 1
 	fi
-	
+
+	# sed无法处理带有“/”的路径，需要先将 "/"变成"\/"才行。
+	# - 删除同名的
 	sed -ri "/^$1:/d" $MYENV_HOME_CONFIG_MAP_NAME_PATH
-	# 不知道路径怎么放到 sed中。
-	#sed -i "#:$path#d" $MYENV_HOME_CONFIG_MAP_NAME_PATH
-	
+	# - 删除同路径的
+	set -x
+	local sed_path=$(echo $path | sed 's/\//\\\//g')
+	sed -i '/:'${sed_path}'$/d' $MYENV_HOME_CONFIG_MAP_NAME_PATH
+	set +x
+	# - 添加新的
 	echo "$1:$path" >> $MYENV_HOME_CONFIG_MAP_NAME_PATH
+	
+	
 	return 0
 }
 
